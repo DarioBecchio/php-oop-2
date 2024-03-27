23331b6d50db37cedaad0f67cc12c2dca2be4943
Dario Becchio <dabek1986@gmail.com>
2024-03-27 18:55:49 +0100

    Create new extensions for Products: Food, toys, kennel. Also create six new products into database

--- app/models/Products.php
+++ app/models/Products.php
@@ -23,21 +23,50 @@ $this->images = $images;
 class Animals
 
 {
-    public function __construct(public $cat, public $dog)
+    public function __construct(public $species)
     {
-        $this->cat= $cat;
-        $this->dog= $dog;
+        $this->species= $species;
     }
 }
 
-class Products extends Animals 
+class Products
 {
-    public function __construct(public $food, public $toys, public $kennel)
+    public function __construct(public string $descriptions, public string $barcode)
     {
-        $this->food= $food;
-        $this->toys= $toys;
-        $this->kennel = $kennel;
+        $this->descriptions = $descriptions;
+        $this->barcode = $barcode;
     }
 }
 
+class Food extends Products 
+{
+    public function __construct(public string $descriptions, public string $barcode , public string $expiredate , public Animals $type)
+    {
+        parent::__construct($descriptions,$barcode);
+        $this->expiredate = $expiredate;
+        $this->type = $type;
+
+    }
+}
+
+class Toys extends Products
+{
+    public function __construct(public string $descriptions, public string $barcode , public string $material , public Animals $type)
+    {
+        parent::__construct($descriptions,$barcode);
+        $this->material = $material;
+        $this->type = $type;
+
+    }
+}
+
+class Kennels extends Products
+{
+    public function __construct(public string $descriptions, public string $barcode , public string $size , public Animals $type)
+    {
+        parent::__construct($descriptions,$barcode);
+        $this->size = $size;
+        $this->type = $type;
+    }
+}
 
--- database/Db.php
+++ database/Db.php
@@ -1,15 +1,11 @@
 <?php
 
-/*
-$product_1 = new Product('Dog', 'Prescription Diet Croquettes', 15.99 , 'Food', 'https://arcaplanet.vtexassets.com/arquivos/ids/270797/Monge-All-Breeds-Adult-Salmone-e-Riso-12Kg.jpg?v=637852830908370000');
-$product_2 = new Product('Cat', 'Science Plan Sterilised Wet food', 25.99 , 'Food', 'https://arcaplanet.vtexassets.com/arquivos/ids/266561/felix-le-ghiottonerie-multipack-80x85g.jpg?v=637780975752670000');
-$product_3 = new Product('Dog','Hairy Donut Kennel', 35.99 , 'Kennel','https://arcaplanet.vtexassets.com/arquivos/ids/225719/mast-brandina-alluminio-60-blu.jpg?v=637454754963900000');
-$product_4 = new Product('Cat','Cat Wand Game', 3.99 , 'Toys','https://arcaplanet.vtexassets.com/arquivos/ids/273114/LOVEDI-GIOCO-GATTO-CANNA-DA-PESCA-CON-PESCE-CM.42.jpg?v=637921027967030000');
-
-
-$products = [$product_1,$product_2,$product_3,$product_4];
-
-*/
-
-$products_1 = new Products('croquette' , 'wand', 'small sized');
-var_dump($products_1);
+$animal_cat = new Animals('cat');
+$animal_dog = new Animals('dog');
+$food_cat_1 = new Food('crocchette' , 'IT1234', '12/07/2025' , $animal_cat);
+$food_dog_1 = new Food('crocchette' , 'IT0123', '12/07/2025' , $animal_dog);
+$toys_cat_1 = new Toys('fish-wand' , 'IT2345', 'plastic' , $animal_cat);
+$toys_dog_1 = new Toys('wood-bone' , 'IT3456', 'wood' , $animal_dog);
+$kennels_cat_1 = new Kennels('penthouse' , 'IT4567', '30x40x10' , $animal_cat);
+$kennels_dog_1 = new Kennels('special-cave' , 'IT5678', '120x70x20' , $animal_dog);
+var_dump($food_cat_1,$food_dog_1,$toys_cat_1,$toys_dog_1,$kennels_cat_1,$kennels_dog_1);
